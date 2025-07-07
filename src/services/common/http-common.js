f15e27b4f30da4450848f3052440aa1292b3602e
deeppaz <deeppaz@hotmail.com>
2025-07-08 02:13:23 +0300

    cors err update

--- src/services/common/http-common.js
+++ src/services/common/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.tempmail.lol/v2/",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://api.tempmail.lol/v2/",
   headers: {
     "Content-type": "application/json",
     "Authorization": "Bearer tempmail.20250708.ldbklx01cdtjk82oeat46hlm9b6e6oozsxcrv0swzei8ewee",
